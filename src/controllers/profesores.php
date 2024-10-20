17015d0d3cfb375e9f059b9c358ea59aa3609b99
Dani03082004 <danibaneza25@gmail.com>
2024-10-20 17:20:42 +0200

    first commit

---
+++ src/controllers/profesores.php
@@ -0,0 +1,4 @@
+<?php
+
+    
+    require VIEWS.'/../views/form.profesores.php';
---
+++ src/views/partials/footer.view.php
@@ -0,0 +1,7 @@
+<footer class="bg-red-600 text-white">
+    <div class="px-3 py-4 flex justify-center">
+        <p class="text-xs">© 2024 Activitat Formularis. Tots els drets reservats.</p>
+    </div>
+</footer>
+</body>
+</html>
